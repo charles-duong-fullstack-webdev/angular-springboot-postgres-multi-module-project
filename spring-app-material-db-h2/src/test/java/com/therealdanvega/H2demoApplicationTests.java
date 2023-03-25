a3934a3ab0e7c934a56765fc2de7e89d28a6edfe
isc-duc <charles.duong@isc-ejpd.admin.ch>
2023-03-25 10:14:45 +0100

    add spring crud

---
+++ spring-app-material-db-h2/src/test/java/com/therealdanvega/H2demoApplicationTests.java
@@ -0,0 +1,24 @@
+package com.therealdanvega;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringApplicationConfiguration(classes = Application.class)
+//@WebAppConfiguration
+//public class H2demoApplicationTests {
+//
+//	@Test
+//	public void contextLoads() {
+//	}
+//
+//}
+
+
+@SpringBootTest
+class Demo2ApplicationTests {
+
+    //@Test
+    void contextLoads() {
+    }
+
+}
