e9f717fe6680d65ce9d9d65df9dcd4d009777ea4
isc-duc <charles.duong@isc-ejpd.admin.ch>
2023-03-30 08:20:54 +0200

    add spring crud

--- spring-app-db-h2-console/src/test/java/com/db/h2/console/H2demoApplicationTests.java
+++ spring-app-db-h2-console/src/test/java/com/db/h2/console/H2demoApplicationTests.java
@@ -6,18 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@SpringApplicationConfiguration(classes = Application.class)
-//@WebAppConfiguration
-//public class H2demoApplicationTests {
-//
-//	@Test
-//	public void contextLoads() {
-//	}
-//
-//}
 
 
 @SpringBootTest
@@ -33,22 +23,10 @@ class H2demoApplicationTests {
 
     @Test
     public void givenGenericEntityRepository_whenSaveAndRetreiveEntity_thenOK() {
-//        Author author = new Author("ffff","ddddd","fgfgfgf");
-//        Post newPost = new Post();
-//        newPost.setActive(true);
-//        newPost.setPostedOn(new Date());
-//        newPost.setAuthor(author);
-//        newPost.setSlug("slug");
-//        newPost.setBody("body");
-//        newPost.setKeywords(null);
-//        newPost.setTitle("title");
-//        Post post = postRepository.save(newPost);
-
-//        Post post = postRepository.findBySlug("grails-is-awesome");
-//
-//        assertNotNull(post);
-//        //assertEquals(post.getAuthor(), foundEntity.get.getValue());
-//        System.out.println(post.toString());
+        Post post = postRepository.findBySlug("grails-is-awesome");
+        assertNotNull(post);
+        assertEquals(post.getAuthor().toString(), "Author [firstName=Dan, lastName=Vega]");
+        System.out.println("===> postRepository.findBySlug(\"grails-is-awesome\"): " + post.toString());
     }
 
 }
