5218c94bf36409b7bad6b2744cce5b0c27331094
isc-duc <charles.duong@isc-ejpd.admin.ch>
2023-05-03 18:34:21 +0200

    delete PerspmExcercise

--- spring-app-db-h2-console/src/test/java/com/db/h2/console/H2demoApplicationTests.java
+++ spring-app-db-h2-console/src/test/java/com/db/h2/console/H2demoApplicationTests.java
@@ -1,21 +1,40 @@
 package com.db.h2.console;
 
+import com.db.h2.console.domain.PersonExercise;
 import com.db.h2.console.domain.Post;
+import com.db.h2.console.repository.PersonExerciseRepository;
 import com.db.h2.console.repository.PostRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
-@SpringBootTest
+@SpringBootTest()
+@TestPropertySource(locations="classpath:application-test.properties")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+//@EnableConfigurationProperties
+//@TestPropertySource(locations= "classpath:application-test.properties")
+@Sql({ "/scripts/schema-h2.sql"})
+@Sql(value  = "/scripts/data-h2.sql")
+@ExtendWith(SpringExtension.class)
 class H2demoApplicationTests {
 
     @Autowired
     private PostRepository postRepository;
 
+    @Autowired
+    private PersonExerciseRepository personExerciseRepository;
     @Test
     void contextLoads() {
     }
@@ -27,6 +46,14 @@ class H2demoApplicationTests {
         assertNotNull(post);
         assertEquals(post.getAuthor().toString(), "Author [firstName=Dan, lastName=Vega]");
         System.out.println("===> postRepository.findBySlug(\"grails-is-awesome\"): " + post.toString());
+    }
+
+    @Test
+    public void givenPersonExerciseEntityRepository_whenSaveAndRetreiveEntity_thenOK() {
+        List<PersonExercise> personExerciseList = personExerciseRepository.findAll();
+        assertNotNull(personExerciseList);
+        assertEquals(personExerciseList.get(0).getCountry(), "swiss");
+        System.out.println("===> personExerciseList.get(0)" + personExerciseList.get(0).toString());
     }
 
 }
